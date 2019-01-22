561053b277919ce964ae84714b06943e9bfbc9e3
Andrey <ivanov.a.e@icloud.com>
2019-01-22 16:54:21 +0600

    add redux-dev-tools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux'
 import { rootReducer } from '../reducers/rootReducer'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 
-export const store = createStore(rootReducer, applyMiddleware(thunk, logger))
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, logger)))
